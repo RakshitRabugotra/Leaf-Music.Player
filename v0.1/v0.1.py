34887411f65be5df64e7c6534ec0142801bd4e74
Rakshit Rabugotra <rakshit.rabugotra000@gmail.com>
2021-09-13 15:59:43 +0530

    Improved v0.1 -> Added Quit Button, Fixed Play/Pause and Quit function issues, Add specific extension choice on loading files

--- v0.1/v0.1.py
+++ v0.1/v0.1.py
@@ -1,6 +1,6 @@
 # Modules for UI
 from tkinter import *
-from tkinter import filedialog
+from tkinter import filedialog, messagebox
 # Modules for backend
 import time
 import pygame
@@ -9,13 +9,18 @@ from mutagen import mp3 as mp
 
 
 BACKGROUND_COLOR = "#001B2B"
-FOREGROUND_COLOR = "#A61651"
 
-BUTTON_BACKGROUND_COLOR = "#4a62d9"
+BUTTON_BACKGROUND_COLOR = "#0E3D8B"
+BUTTON_FOREGROUND_COLOR = "#2B8EE2"
+
+STATUS_FOREGROUND_COLOR = "#FFFFFE"
+STATUS_BACKGROUND_COLOR = "#001B2B"
+
+BUTTON_BORDER_WIDTH = 0
 
 """
-FOREGROUND -> #301869, #6326e2
-BACKGROUND -> #134b72, #848c8c, #4a62d9
+FOREGROUND -> #301869, #6326e2, 
+BACKGROUND -> #134b72, #848c8c, #4a62d9, #001B2B
 """
 
 
@@ -35,6 +40,9 @@ class MusicPlayer:
         # Integers
         self.audioLength = 0
 
+        # MP3 and mixer obejcts
+        self.mp3 = None
+
         # Strings
         self.defaultStatus = "Load a file to play!"
         self.playingTime = "--:--:--"
@@ -42,9 +50,14 @@ class MusicPlayer:
         self.audioFileTitle = ""
 
         # Setting up Buttons
-        Load = Button(window, relief=GROOVE, borderwidth=0, activebackground=FOREGROUND_COLOR, text='Load', width=12, bg=BUTTON_BACKGROUND_COLOR, fg=FOREGROUND_COLOR, font=('Helvetica', 12, 'bold'), command=self.load)
-        Pause = Button(window, relief=GROOVE, borderwidth=0, activebackground=FOREGROUND_COLOR, text='Pause/Play', bg=BUTTON_BACKGROUND_COLOR, width=12, fg=FOREGROUND_COLOR, font=('Helvetica', 12, 'bold'), command=self.pause)
-        Stop = Button(window, relief=GROOVE, borderwidth=0, activebackground=FOREGROUND_COLOR, text='Stop', width=12, bg=BUTTON_BACKGROUND_COLOR, fg=FOREGROUND_COLOR, font=('Helvetica', 12, 'bold'), command=self.stop)
+        Load = Button(window, relief=GROOVE, borderwidth=BUTTON_BORDER_WIDTH, activebackground=BUTTON_FOREGROUND_COLOR, text='Load', width=12, bg=BUTTON_BACKGROUND_COLOR, fg=BUTTON_FOREGROUND_COLOR, font=('Helvetica', 12, 'bold'), command=self.load)
+        Pause = Button(window, relief=GROOVE, borderwidth=BUTTON_BORDER_WIDTH, activebackground=BUTTON_FOREGROUND_COLOR, text='Pause/Play', width=12, bg=BUTTON_BACKGROUND_COLOR, fg=BUTTON_FOREGROUND_COLOR, font=('Helvetica', 12, 'bold'), command=self.pause)
+        Stop = Button(window, relief=GROOVE, borderwidth=BUTTON_BORDER_WIDTH, activebackground=BUTTON_FOREGROUND_COLOR, text='Stop', width=12, bg=BUTTON_BACKGROUND_COLOR, fg=BUTTON_FOREGROUND_COLOR, font=('Helvetica', 12, 'bold'), command=self.stop)
+
+        # Quit Button
+        Button(window, relief=GROOVE, borderwidth=BUTTON_BORDER_WIDTH, activebackground=BUTTON_FOREGROUND_COLOR, text='Quit', width=34, bg=BUTTON_BACKGROUND_COLOR, fg=BUTTON_FOREGROUND_COLOR, font=('Helvetica', 12, 'bold'), command=lambda:self.window.quit() if messagebox.askyesno("Quit to Windows", "Do you want to exit?") else "").place(x=42, y=56)
+
+        # Placing the buttons
         Load.place(x=30, y=24)
         Pause.place(x=156, y=24)
         Stop.place(x=276, y=24)
@@ -52,17 +65,18 @@ class MusicPlayer:
         self.Load, self.Pause, self.Stop = Load, Pause, Stop
 
         # Setting up Status Label
-        self.StatusLabel = Label(window, text=self.defaultStatus, fg=FOREGROUND_COLOR, font=('Helvetica', 12, 'bold'), anchor=W)
+        self.StatusLabel = Label(window, text=self.defaultStatus, bg=STATUS_BACKGROUND_COLOR, fg=STATUS_FOREGROUND_COLOR, font=('Helvetica', 12, 'italic'), anchor=W)
         self.StatusLabel.pack(side=BOTTOM, fill=X)
 
         # Deciding the state of buttons
         self.decideButtonState()
 
     def load(self):
-        self.musicFile = filedialog.askopenfilename()
+        self.musicFile = filedialog.askopenfilename(filetypes=[("Audio file (.MP3/ .OGG/ .WAV)", ".mp3 .ogg .wav"), ("All files", "*")])
         if self.musicFile:
             # Initializing Audio File
             mp3 = mp.MP3(self.musicFile)
+            self.mp3 = mp3
             mixer.init(frequency=mp3.info.sample_rate)
 
             # Getting the title of audio file
@@ -100,21 +114,39 @@ class MusicPlayer:
         self.timeElapsed()
 
     def pause(self):
-        if not self.playingState:
+        if self.playingState:
             mixer.music.pause()
-            self.playingState = True
-            self.Pause['text'] = "Pause"
-        else:
-            mixer.music.unpause()
             self.playingState = False
             self.Pause['text'] = "Play"
+            self.window.title(self.audioFileTitle+" (Paused)")
+        else:
+            if mixer.music.get_pos()//1000 == -1:
+                # The Music has stopped and file should be rewinded (begin from start 00:00:00)
+                mixer.music.rewind()
+                # Playing the audio file
+                mixer.music.play()
+            else:
+                # Try unpausing the file if it is not playing
+                mixer.music.unpause()
+
+            self.playingState = True
+            self.Pause['text'] = "Pause"
+            self.window.title(self.audioFileTitle)
 
     def stop(self):
         # Stopping the mixer
         mixer.music.stop()
+        if mixer.music.get_pos()//1000 == -1:
+            # Changin the playing state var
+            self.playingState = False
+            # Changing the text on Label to 'Play'
+            self.Pause['text'] = 'Play'
+            # Changing the status to Stopped
+            self.changeStatus(msg="00:00:00/"+self.totalTime+" (Music Stopped)", after=False)
+
 
     def timeElapsed(self):
-        try:
+        if mixer.music.get_pos() != -1:
             # Fetching time elapsed playing the audio file
             currentTime = mixer.music.get_pos() // 1000
             # Converting the time fetch (in 'int') to string like "HH:MM:SS"
@@ -125,12 +157,13 @@ class MusicPlayer:
             # Displaying play time on screen using status label
             self.changeStatus(msg=duration, after=False)
 
-            # Increase current time by 1 second
-            currentTime += 1
-
-            if currentTime != self.audioLength: self.window.after(1000, self.timeElapsed)
-        except pygame.error:
-            return
+            if self.playingState:
+                # Increase current time by 1 second
+                currentTime += 1
+            else:
+                self.changeStatus(msg=duration+" (Music Paused)", after=False)
+        self.window.after(1000, self.timeElapsed)
+        
 
     def changeStatus(self, _type=None, msg=None, after=True, after_text=None):
         if msg is None: msg = self.defaultStatus
